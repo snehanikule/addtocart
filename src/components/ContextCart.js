bc103e45470e688eb4b3b11000c378f1442f17f1
sneha <snehanikule93@gmail.com>
2023-01-10 23:06:41 +0530

    cart

---
+++ src/components/ContextCart.js
@@ -0,0 +1,67 @@
+import React, { useContext } from 'react'
+import { Scrollbars } from 'react-custom-scrollbars-2';
+import Items from './Items';
+
+import { cartContext } from './Cart';
+
+const ContextCart = () => {
+
+    const { item, totalAmount, totalItem, clearCart } = useContext(cartContext);
+    if (item.length === 0) {
+        return (
+            <>
+                <section className="main-cart-section">
+                    <h1>Your Cart</h1>
+                    <p className="total-items">
+                        Repair Devices <span className="total-items-count">0 </span>
+                    </p>
+
+                    <div className="cart-items">
+                        <div className="cart-items-container">
+                            <Scrollbars className="cart-items-container">
+                                <h1>Empty Cart</h1>
+                            </Scrollbars>
+                        </div>
+                    </div>
+                </section>
+            </>
+        )
+    }
+    return (
+        <div>
+            <header>
+                <div className="continue-shopping">
+                    <h3>Application Task:</h3>
+                </div>
+                <div className="cart-icon">
+                    <img src="./images/cart.png" alt="cart-logo" />
+                    <p>{totalItem}</p>
+                </div>
+            </header>
+            <section className='main-cart-section'>
+                <h1>Your cart</h1>
+                <button onClick={clearCart}>Clear This Cart</button>
+                <p className='cart-device'>Devices</p>
+                <p className='total-items'>Repair Devices (<span className='total-items-count'>{totalItem} </span>Devices)</p>
+
+                <div className='cart-items'>
+                    <div className='cart-items-container'>
+                        <Scrollbars>
+                            {
+                                item.map((currItem) => {
+                                    return <Items key={currItem.id} {...currItem} />
+                                })
+                            }
+                        </Scrollbars>
+                    </div>
+                </div>
+                <div className='card-total'>
+                    <h3>Total Amount : <span>₹{totalAmount} </span></h3>
+                    <button>Checkout Service</button>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default ContextCart
